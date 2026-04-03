99ea943e43722cb06ddd87cc7a45d1d87b1b7d0e
JuaniAvverame <juaniavverame@gmail.com>
2026-04-03 12:39:38 -0300

    Agregar Dockerfile para Render

--- obj/Debug/net8.0/WhatsAppWebhook.AssemblyInfo.cs
+++ obj/Debug/net8.0/WhatsAppWebhook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhatsAppWebhook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d7d7b69b02ce27e3268acc83255ade2332a7c7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhatsAppWebhook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhatsAppWebhook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
